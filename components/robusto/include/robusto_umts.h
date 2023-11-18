b4e3fdb5edd6d2d1138486637aaf81385506280e
Nicklas Börjesson <nicklasb@gmail.com>
2023-11-18 11:35:56 +0100

    Publicize get_sync_attempts

--- components/robusto/include/robusto_umts.h
+++ components/robusto/include/robusto_umts.h
@@ -41,6 +41,7 @@ rob_ret_val_t robusto_umts_mqtt_publish(char * topic, char *data);
 rob_ret_val_t robusto_umts_oauth_post_form_multipart(char *url, char *data, uint16_t data_len, char *context_type, char *name, char* parent);
 rob_ret_val_t robusto_umts_oauth_post_urlencode(char *url, char *data, uint16_t data_len);
 
+int get_sync_attempts();
 
 bool robusto_umts_sms_up();
 bool robusto_umts_mqtt_up();
