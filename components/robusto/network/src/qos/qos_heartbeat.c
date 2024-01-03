2c73ed15cc47d13d207416df0f126a3f8a886d1d
Nicklas Börjesson <nicklasb@gmail.com>
2024-01-03 23:40:00 +0100

    Remove waiting for receipts for heartbeats

--- components/robusto/network/src/qos/qos_heartbeat.c
+++ components/robusto/network/src/qos/qos_heartbeat.c
@@ -113,15 +113,11 @@ void send_heartbeat_message(robusto_peer_t *peer, e_media_type media_type)
         queue_state *q_state = NULL;
         q_state = robusto_malloc(sizeof(queue_state));
         rob_ret_val_t send_ret_val;
+        // Heartbeats are one-way and doesn't wait for receipts
         rob_ret_val_t queue_ret_val = send_message_raw_internal(peer, media_type, hb_msg, hb_msg_len, q_state, false, true, 0, robusto_mt_none);
-        // TODO: Perhaps a catchall handler of these results would be nice
         if (queue_ret_val != ROB_OK) {
             // If we get a problem here, there might be an internal issue, it is immidiately considered a problem.
             ROB_LOGE(heartbeat_log_prefix, "Early error sending heartbeat to %s, mt %hhu, res %hi, ", peer->name, (uint8_t)media_type, queue_ret_val);
-        } else         
-        if (!robusto_waitfor_queue_state(q_state, 6000, &send_ret_val)) {
-            set_state(peer, info, media_type, media_state_problem, media_problem_send_problem);
-            ROB_LOGE(heartbeat_log_prefix, "Failed sending heartbeat, to %s, mt %hhu, queue state %hhu , reason code: %hi", peer->name, media_type, *q_state[0], send_ret_val);
         } 
         q_state = robusto_free_queue_state(q_state);
     }
