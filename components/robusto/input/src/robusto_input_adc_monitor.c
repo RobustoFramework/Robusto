1764ad8b9ff83a992c35636a72f816bda73c069c
Nicklas Börjesson <nicklasb@gmail.com>
2024-01-31 01:40:30 +0100

    Add conditional config for ESP32 and ESP32S2

--- components/robusto/input/src/robusto_input_adc_monitor.c
+++ components/robusto/input/src/robusto_input_adc_monitor.c
@@ -1,6 +1,7 @@
 #include "robusto_input_init.h"
 #include <robusto_logging.h>
 #include <robusto_repeater.h>
+#include <inttypes.h>
 
 #ifdef USE_ESPIDF
 #include "freertos/FreeRTOS.h"
@@ -18,7 +19,7 @@ static char _monitor_name[16] = "Adc monitor\x00";
 
 recurrence_t memory_monitor = {
     recurrence_name : &_monitor_name,
-    skip_count : 0,
+    skip_count : 10,
     skips_left : 0,
     recurrence_callback : &monitor_adc_cb,
     shutdown_callback : &monitor_adc_shutdown_cb
@@ -40,18 +41,26 @@ recurrence_t memory_monitor = {
 
 #endif
 
+#if defined(ADC1_CHANNEL_4)
+
 static int adc_raw[2][10];
 
 static esp_adc_cal_characteristics_t adc1_chars;
 static esp_adc_cal_characteristics_t adc2_chars;
 
+bool cali_enabled = false;
+
 void monitor_adc_cb() {
-    
- 
+    uint32_t voltage = 0;
+    int adc_raw = adc1_get_raw(ADC1_CHANNEL_4);
+    if (cali_enabled) {
+        voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc1_chars);
+        ROB_LOGI(adc_monitor_log_prefix, "cali data: %" PRIu32 " mV, raw: %i", voltage, adc_raw);
+    }
 
 };
 void monitor_adc_shutdown_cb() {
-
+    ROB_LOGI(adc_monitor_log_prefix, "Stop!!");
 };
 
 /**
@@ -67,25 +76,28 @@ void robusto_input_start_adc_monitoring() {
 
 #ifdef USE_ESPIDF
 
-static bool adc_calibration_init(void)
+void adc_calibration_init(void)
 {
     esp_err_t ret;
-    bool cali_enable = false;
-
+    cali_enabled = false;
+    ROB_LOGI(adc_monitor_log_prefix, "Calibrating ADCs");
     ret = esp_adc_cal_check_efuse(ADC_MONITOR_CALI_SCHEME);
     if (ret == ESP_ERR_NOT_SUPPORTED) {
         ROB_LOGI(adc_monitor_log_prefix, "Calibration scheme not supported, skip software calibration");
     } else if (ret == ESP_ERR_INVALID_VERSION) {
         ROB_LOGI(adc_monitor_log_prefix, "eFuse not burnt, skip software calibration");
     } else if (ret == ESP_OK) {
-        cali_enable = true;
+        cali_enabled = true;
         esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
         esp_adc_cal_characterize(ADC_UNIT_2, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc2_chars);
     } else {
         ROB_LOGI(adc_monitor_log_prefix, "Invalid arg");
     }
-
-    return cali_enable;
+    ROB_LOGI(adc_monitor_log_prefix, "Done Calibrating ADCs");
+    // TODO
+    //ADC1 config
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11));
 }
 
 #endif
