e62b5012d01b714436a6405bcda6230cc5af1803
Nicklas Börjesson <nicklasb@gmail.com>
2024-04-07 22:03:21 +0200

    Proper handling of incoming message for I2C

--- test/tst_i2c_message_receiving.h
+++ test/tst_i2c_message_receiving.h
@@ -6,4 +6,3 @@ void tst_message_receive_binary_message_mock(void);
 void tst_message_receive_multi_message_mock(void);
 void tst_message_receive_binary_message_restricted_mock(void);
 void tst_i2c_message_receive_presentation(void);
-void tst_i2c_message_receive_string_message(void);
