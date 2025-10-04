23ad217177cba296cd3451df2d5e3c6c69622a44
nicklasb <nicklasb@gmail.com>
2025-10-05 00:42:29 +0200

    Add description

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Publish Robusto framework and component
+# HOWTO:
+# * Commit all changes
+# * Update version in components/robusto/idf_component.yml and library.json
+# * Create a commit with message "Tick up or similar
+# * Run this script
+# * Create a tag with the same version as in idf_component.yml and library.json
 pio pkg publish --type library --owner robusto
 cd components/robusto/
 compote component upload --namespace robusto --name Robusto
